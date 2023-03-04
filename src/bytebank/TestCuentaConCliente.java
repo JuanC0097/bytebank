699e1fdf6c1df10c274eb8aa877e4a4c0c1d07c1
JuanC0097 <juanvillarq0097@gmail.com>
2023-03-03 20:56:35 -0500

    Creacion de ejemplo con metodos getter y setter, llamado desde clase cuenta y cliente

---
+++ src/bytebank/TestCuentaConCliente.java
@@ -0,0 +1,24 @@
+package bytebank;
+
+public class TestCuentaConCliente {
+
+	public static void main(String[] args) {
+		//Creacion Cliente
+		Cliente juan = new Cliente();
+		//Operaciones
+		juan.setNombre("Juan");
+		juan.setDocumento("35418615561");
+		juan.setTelefono("351 548 1458");
+		
+		//Creacion Cuenta
+		Cuenta cuentaJuan = new Cuenta(777, 1);
+		//Operaciones
+		cuentaJuan.depositar(1000);
+		
+		//cuentaJuan.titular = juan
+		cuentaJuan.setTitular(juan);
+		System.out.println(cuentaJuan.getTitular().getNombre());
+		
+	}
+	
+}
